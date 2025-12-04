9ffd570515bdd9500cbaa8b2bd0f4704b2010e07
Juven14s <129730169+Juven14s@users.noreply.github.com>
2025-12-04 11:10:13 +0100

    fix

--- test/workout_test.jl
+++ test/workout_test.jl
@@ -1,8 +1,6 @@
 # examples/workout_test.jl
 
 # On inclut le fichier principal (chemin relatif depuis le dossier examples/)
-# Si vous lancez julia depuis la racine du projet, adaptez le chemin.
-# Ici, on suppose que le fichier est dans le dossier examples.
 include("../src/Samo.jl") 
 
 # On utilise le module principal
@@ -11,7 +9,6 @@ using .Samo
 println("=== Démarrage du Test SamoGym ===")
 
 # 1. Création de la session vide
-# Notez le typage : Vector{AbstractActivity} permet d'accueillir n'importe quoi !
 session = WorkoutSession("Jeudi 14 Mars", AbstractActivity[])
 
 # 2. Création des exercices (Instanciation)
@@ -20,7 +17,6 @@ session = WorkoutSession("Jeudi 14 Mars", AbstractActivity[])
 squat = StrengthExercise("Squat", 100, 5, 5, 8) 
 
 # Cas B : Musculation avec un flottant (Float64) et PAS de RPE (nothing)
-# Grâce au paramètre {T <: Real}, 82.5 est accepté sans changer la structure.
 bench = StrengthExercise("Bench Press", 82.5, 8, 3, nothing)
 
 # Cas C : Cardio
@@ -36,5 +32,4 @@ println("   (Le vecteur contient un mélange de Strength et Cardio !)")
 println()
 
 # 4. Test de la Logique et de l'Affichage
-# Cette fonction va appeler votre 'show' personnalisé et vos calculs.
 summarize(session)
